5f91fd4523453edd007e268b257948eccf1fda54
Rimubo <zmisyura1997@gmail.com>
2021-03-06 23:17:27 +0200

    Homework52

---
+++ src/task1.java
@@ -0,0 +1,13 @@
+public class task1 {
+    public static void main(String[] args) {
+        int n=5;
+        if(n%2==0){
+            System.out.println("Число "+n+" - четное");
+        }
+        else {
+            System.out.println("Число "+n+" - нечетное");
+        }
+    }
+}
+
+
